5a64338ec8e01307bc98fecfcb702c03f8cef370
manoj-nathwani <manoj_1993@hotmail.com>
2021-10-29 10:36:54 +0000

    cleanup main.py

--- navigator_api/main.py
+++ navigator_api/main.py
@@ -1,9 +1,10 @@
 import logging
-import time
+from random import randint
 from flask import Blueprint, jsonify, session
 from flask_login import login_required
 from webargs import fields
 from webargs.flaskparser import use_args
+from lorem import get_paragraph, get_sentence
 
 main_blueprint = Blueprint('main', __name__)
 
@@ -50,6 +51,51 @@ def datasets():
                     "name": "Antarctica Inputs UNAIDS Estimates 2021"
                 }
             ]
+        }
+    )
+
+
+@main_blueprint.route('/dataset_state/<dataset_id>', methods=['GET'])
+@login_required
+def dataset_state_get(dataset_id):
+    logging.info(f'dataset_id: {dataset_id}')
+    possible_milestones = ['Shiny90', 'Naomi', 'Data Prep']
+    possible_tasks = [
+        "Generate input data",
+        "Go to Spectrum/AIM. Row 34",
+        "Download data templates from ADR",
+        "Populate Shiny90 survey template",
+        "Populate Shiny90 HIV testing template?",
+        "Populate the Population template",
+        "Populate ANC survey template",
+        "Populate ART template",
+        "Populate Naomi survey template",
+        "Populate geo template",
+        "Create user account",
+    ]
+    task_number = randint(2, 85)
+    task_name = possible_tasks[
+        randint(0, len(possible_tasks)-1)
+    ]
+    list_items = ('').join([
+        f'<li>{get_sentence(count=(1, 3))}</li>'
+        for x in range(randint(1, 7))
+    ])
+    html = ''.join([
+        f'<h3>{task_name}</h3>',
+        f'<p>{get_paragraph(count=(3, 5))}</p>',
+        f'<ol>{list_items}</ol>'
+    ])
+    return jsonify(
+        {
+            'task_number': task_number,
+            'milestone': possible_milestones[randint(0, len(possible_milestones)-1)],
+            'display_html': html,
+            'lets_go_url': 'https://google.com',
+            'skippable': bool(randint(0, 1)),
+            'percentage': 5 if task_number < 5 else task_number,
+            'grey': randint(0, 5),
+            'last_updated': int(time.time())
         }
     )
 
