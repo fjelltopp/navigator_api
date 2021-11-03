0798fc80234c77809c7b835fd03abe4516eb7fe9
Tomasz Sabała <tomek@fjelltopp.org>
2021-11-03 20:34:36 +0100

    NAV-49 ckan api get user organizations

--- navigator_api/ckan.py
+++ navigator_api/ckan.py
@@ -14,9 +14,13 @@ def fetch_country_estimates_datasets(ckan_cli):
     response = ckan_cli.action.package_search(q="type:country-estimates-22", rows=100000, hide_inaccessible_resources=True)
     return response["results"]
 
+def fetch_users_organization_ids(ckan_cli):
+    response = ckan_cli.action.organization_list_for_user(permission="read")
+    return [org['id'] for org in response]
+
 
 if __name__ == '__main__':
     app = create_app()
     with app.app_context():
-        ckan = init_ckan(apikey=os.getenv('CKAN_APIKEY'))
+        ckan_cli = init_ckan(apikey=os.getenv('CKAN_APIKEY'))
         pass
