53b45f4e4af0e2f766db507e4ee23f272f9549f5
Tomasz Sabała <tomek@fjelltopp.org>
2021-11-05 16:56:06 +0100

    NAV-48 Test db setup & teardown

--- navigator_api/tests/conftest.py
+++ navigator_api/tests/conftest.py
@@ -1,8 +1,18 @@
 import pytest
 
+from navigator_api import model
+from navigator_api.app import create_app
 from navigator_api.tests import factories
 
 
 @pytest.fixture()
 def workflow():
     return factories.WorklowFactory()
+
+@pytest.fixture(autouse=True, scope="session")
+def setup():
+    app = create_app('navigator_api.config.Testing')
+    with app.app_context():
+        model.db.create_all()
+        yield
+        model.db.drop_all()
