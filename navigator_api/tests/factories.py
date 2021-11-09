2b92018825ab1b75e86afd9098c626a0955491f5
Tomasz Sabała <tomek@fjelltopp.org>
2021-11-09 15:54:59 +0100

    NAV-48 Get all workflows for a user

--- navigator_api/tests/factories.py
+++ navigator_api/tests/factories.py
@@ -1,10 +1,13 @@
 import factory
-from navigator_api import model
+from navigator_api import model as _model
 
 
-class WorklowFactory(factory.Factory):
+class WorklowFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
-        model = model.Workflow
+        model = _model.Workflow
+        sqlalchemy_session = _model.db.session
+        sqlalchemy_get_or_create = ('dataset_id','user_id')
+        sqlalchemy_session_persistence = 'commit'
     id = factory.Sequence(lambda n: int(n))
     decision_engine_id = factory.Faker('uuid4')
     dataset_id = factory.Faker('uuid4')
--- navigator_api/tests/test_model.py
+++ navigator_api/tests/test_model.py
@@ -1,3 +1,7 @@
+from navigator_api.model import get_workflows
+from navigator_api.tests import factories
+
+
 def test_new_workflow_has_empty_skipped_tasks(workflow):
     assert workflow.skipped_tasks == []
 
@@ -13,3 +17,9 @@ def test_workflow_skipping_task(workflow):
     workflow.skipped_tasks = tasks
     workflow.skipped_tasks = workflow.skipped_tasks + ['task_last']
     assert workflow.skipped_tasks[-1] == 'task_last'
+
+
+def test_get_workflows_return_all_for_user(user):
+    factories.WorklowFactory.create_batch(10, user_id=user.id)
+    actual = get_workflows(user_id=user.id)
+    assert len(actual) == 10
