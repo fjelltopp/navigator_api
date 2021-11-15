c8639aceb58d544420da5f0516329c257eff0846
Tomasz Sabała <tomek@fjelltopp.org>
2021-11-15 16:17:15 +0100

    Refactored fixtures for test_app and db init.

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,10 +18,15 @@ def user():
     return User(id=str(uuid.uuid4()))
 
 
-@pytest.fixture(autouse=True, scope="session")
-def setup():
+@pytest.fixture(scope="session")
+def test_app():
     app = create_app('config.Testing')
     with app.app_context():
-        model.db.create_all()
-        yield
-        model.db.drop_all()
+        yield app
+
+
+@pytest.fixture(autouse=True, scope="session")
+def setup(test_app):
+    model.db.create_all()
+    yield
+    model.db.drop_all()
